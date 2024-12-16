f7f55c106bb6c89c77cca569748ef3316eca37f4
ljyGmail <liangjy881@gmail.com>
2024-12-16 17:11:11 +0900

    ch01 setting up python, anaconda and spyder

---
+++ ch01_setting_up_python_anaconda_spyder/my_first_script.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("This is my very first Python script!")
+
+print("This is my second message!")
+
+print(7 * 123)
